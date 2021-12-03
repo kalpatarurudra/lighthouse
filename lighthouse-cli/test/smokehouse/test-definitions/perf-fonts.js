d368521bc5cc549ce82f5c76e46a0996cfaf78cc
Brendan Kenny <bckenny@gmail.com>
2021-12-03 12:34:28 -0600

    smoke: perf

--- lighthouse-cli/test/smokehouse/test-definitions/perf-fonts.js
+++ lighthouse-cli/test/smokehouse/test-definitions/perf-fonts.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright 2019 The Lighthouse Authors. All Rights Reserved.
+ * @license Copyright 2017 The Lighthouse Authors. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
@@ -49,4 +49,47 @@ const config = {
   },
 };
 
-export default config;
+/**
+ * @type {Smokehouse.ExpectedRunnerResult}
+ * Expected Lighthouse audit values for testing font perf.
+ */
+const expectations = {
+  networkRequests: {
+    length: 5,
+  },
+  lhr: {
+    requestedUrl: 'http://localhost:10200/perf/fonts.html',
+    finalUrl: 'http://localhost:10200/perf/fonts.html',
+    audits: {
+      'font-display': {
+        score: 0,
+        details: {
+          items: [
+            {
+              url: 'http://localhost:10200/perf/lobster-v20-latin-regular.woff2',
+            },
+          ],
+        },
+      },
+      'preload-fonts': {
+        scoreDisplayMode: 'notApplicable',
+        // Disabled for now, see https://github.com/GoogleChrome/lighthouse/issues/11960
+        // score: 0,
+        // details: {
+        //   items: [
+        //     {
+        //       url: 'http://localhost:10200/perf/lobster-two-v10-latin-700.woff2?delay=1000',
+        //     },
+        //   ],
+        // },
+      },
+    },
+  },
+};
+
+export default {
+  id: 'perf-fonts',
+  expectations,
+  config,
+  runSerially: true,
+};
